22d273d5533dcb01fcd0775d15408e4dca4935d3
Adriana Hernandez <adrianahe@lstc.lesschwab.com>
2022-07-10 20:10:04 -0700

    suspension_data

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,5 +1,9 @@
-mechacar_data <- read.csv('MechaCar_mpg.csv') #import dataset
+mechacar_data <- read.csv('MechaCar_mpg.csv') #import mechacar dataset
 
 lm(vehicle_length ~ mpg + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = mechacar_data) #create linear model
 
 summary(lm(vehicle_length ~ mpg + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = mechacar_data)) #generate summary statistics
+
+suspension_data <- read.csv('Suspension_Coil.csv') #import suspension dataset
+
+summarize_suspension <- suspension_data %>% summarize(Mean_PSI=mean(PSI),Median_PSI=median(PSI),SD_PSI=sd(PSI),VAR_PSI=var(PSI), .groups = 'keep') #create summary table with multiple columns
