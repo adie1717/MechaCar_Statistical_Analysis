b8a5ae5217cce95aceede1faec944e3a4b943370
Adriana Hernandez <adrianahe@lstc.lesschwab.com>
2022-07-10 20:35:48 -0700

    deliv_3

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -9,3 +9,5 @@ suspension_data <- read.csv('Suspension_Coil.csv') #import suspension dataset
 summarize_suspension <- suspension_data %>% summarize(Mean_PSI=mean(PSI),Median_PSI=median(PSI),SD_PSI=sd(PSI),VAR_PSI=var(PSI), .groups = 'keep') #create summary table with multiple columns
 
 summarize_lots <- suspension_data %>% group_by(Manufacturing_Lot) %>% summarize(Mean_PSI=mean(PSI),Median_PSI=median(PSI),SD_PSI=sd(PSI),VAR_PSI=var(PSI), .groups = 'keep') #create lot summary table with multiple columns
+
+t.test(log10(summarize_lots$Median_PSI),mu=mean(log10(summarize_lots$Mean_PSI))) #compare sample PSI vs population means
