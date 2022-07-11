54245111e9e6f5ec87f41d336f18aecf9dec1b68
Adriana Hernandez <adrianahe@lstc.lesschwab.com>
2022-07-10 19:24:24 -0700

    mechacar_challenge2

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,3 +1,5 @@
 mechacar_data <- read.csv('MechaCar_mpg.csv') #import dataset
 
 lm(vehicle_length ~ mpg + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = mechacar_data) #create linear model
+
+summary(lm(vehicle_length ~ mpg + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = mechacar_data)) #generate summary statistics
